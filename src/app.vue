fc94726fbf3ddb1df8579faea92f763cdb99d7b4
fattydevelop <doub7boss@163.com>
2015-09-15 23:42:14 +0800

    update

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<style>
+  .red {
+    color: #f00;
+  }
+</style>
+
+<template>
+  <h1 class="red">{{msg}}</h1>
+   <a v-link="{ path: '/foo' }">Go to Foo</a>
+
+</template>
+<script>
+  module.exports = {
+    data: function () {
+      return {
+        msg: 'Hello worrrrrld!'
+      }
+    }
+  }
+</script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,11 @@
 module.exports = {
-  entry: "./src/main.js",
-  output: {
-    path: "./build",
-    publicPath: "/build/",
-    filename: "build.js"
-  },
-  module: {
-    loaders: [
-      { test: /\.styl$/, loader: "style!css!stylus" },
-      { test: /\.html$/, loader: "html" }
-    ]
-  }
+    entry: './src/main.js',
+    output: {
+        filename: './build/build.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.vue$/, loader: 'vue' },
+        ]
+    }
 }
