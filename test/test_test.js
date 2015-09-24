e43302d68d2daf4f28773667e2df0ceac9aa1b06
fattydevelop <doub7boss@163.com>
2015-09-25 02:31:43 +0800

    add test karma bug exsit

---
+++ test/test_test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by doub on 2015/9/25 0025.
+ */
+var assert = require("assert");
+
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+
+        });
+    });
